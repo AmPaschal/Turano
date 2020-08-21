5c70e744e51b8ce1a58dfde85af99879d793f01c
Chukwuebuka <muofunanya3@gmail.com>
2020-08-21 12:37:44 +0100

    Rectified issue in fragment_places.xml and list_places_item.xml

---
+++ app/src/main/java/com/ampaschal/soilinfo/ui/FirebaseUpdateActivity2.java
@@ -0,0 +1,16 @@
+package com.ampaschal.soilinfo.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ampaschal.soilinfo.R;
+
+public class FirebaseUpdateActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase_update2);
+    }
+}
